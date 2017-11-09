d444f2d700371728c600751fd613dad350ced5f5
guillaume crouillere <gcrouillere@hotmail.fr>
2017-11-09 11:28:56 +0100

    modif on links for SEO

--- app/assets/javascripts/removableinfobulles.js
+++ app/assets/javascripts/removableinfobulles.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('.cross-1, .cross-mobile').on('click', function(e) {
     $('.list-header-1').remove();
-    $('.h1-home').remove();
-    $('.h1-home-mobile').remove();
+    $('.h1-home-2').remove();
   });
   $('.cross-2 ').on('click', function(e) {
-    $('.h1-home-2').remove();
+    $('.h1-home').remove();
+    $('.h1-home-mobile').remove();
     $('.list-header-2').remove();
   });
 });
