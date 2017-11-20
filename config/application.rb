42c0352589421ab0518f491e7e64a5e513c178f3
guillaume crouillere <gcrouillere@hotmail.fr>
2017-11-20 17:33:06 +0100

    typo on rackrewrite in app.rb 2

--- config/application.rb
+++ config/application.rb
@@ -24,16 +24,16 @@ module Ceramiquesnugier
       generate.helper false
     end
 
-    config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-      r301      '/', 'https://www.creermonecommerce.fr/'
-      r301      '/home', 'https://www.creermonecommerce.fr/home'
-      r301      '/info', 'https://www.creermonecommerce.fr/info'
-      r301      '/produits', 'https://www.creermonecommerce.fr/produits'
-      r301      '/lessons/new', 'https://www.creermonecommerce.fr/lessons/new'
-      r301      '/#anchor-info', 'https://www.creermonecommerce.fr/#anchor-info'
-      r301      '/users/sign_in', 'https://www.creermonecommerce.fr/users/sign_in'
-      r301      '/users/sign_up', 'https://www.creermonecommerce.fr/users/sign_up'
-    end
+    # config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+    #   r301      '/', 'https://www.creermonecommerce.fr/'
+    #   r301      '/home', 'https://www.creermonecommerce.fr/home'
+    #   r301      '/info', 'https://www.creermonecommerce.fr/info'
+    #   r301      '/produits', 'https://www.creermonecommerce.fr/produits'
+    #   r301      '/lessons/new', 'https://www.creermonecommerce.fr/lessons/new'
+    #   r301      '/#anchor-info', 'https://www.creermonecommerce.fr/#anchor-info'
+    #   r301      '/users/sign_in', 'https://www.creermonecommerce.fr/users/sign_in'
+    #   r301      '/users/sign_up', 'https://www.creermonecommerce.fr/users/sign_up'
+    # end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
