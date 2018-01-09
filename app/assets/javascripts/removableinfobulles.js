ced40e216233dba4d868838ecae700c956d81f0b
guillaume crouillere <gcrouillere@hotmail.fr>
2018-01-09 13:19:44 +0100

    fix error on closable popup on hoempage in js

--- app/assets/javascripts/removableinfobulles.js
+++ app/assets/javascripts/removableinfobulles.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('.cross-1, .cross-mobile').on('click', function(e) {
     $('.list-header-1').remove();
     $('.h1-home-2').remove();
+    $('.h1-home-mobile').remove();
   });
   $('.cross-2 ').on('click', function(e) {
     $('.h1-home').remove();
